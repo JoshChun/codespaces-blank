f2999711bfe463ec21168cc9d8d332a7ea10027f
Joshua Chun <joshuachun@outlook.com>
2025-03-05 16:55:05 +1100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    subscription_id = "03e50e49-b267-403d-9df3-0533921b505e"
     resource_group_name   = "rg-tfstate"
     storage_account_name  = "sttfstate5716"
     container_name        = "tfstate"
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,4 +9,5 @@ terraform {
 
 provider "azurerm" {
     features {}
+    subscription_id = "03e50e49-b267-403d-9df3-0533921b505e"
 }
